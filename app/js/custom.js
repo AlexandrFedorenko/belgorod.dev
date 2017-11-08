0ebeff285eb955d7208f6ee2fecc7df791a8f682
Alexandr <alexandrfedorenkoooo@gmail.com>
2017-11-08 16:42:50 +0200

    add index top

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,41 +1,32 @@
 $(function() {
+    // no dragstart img
+    $("img, a").on("dragstart", function(event) { event.preventDefault(); });   
+});
 
-    //SVG Fallback
-    if(!Modernizr.svg) {
-        $("img[src*='svg']").attr("src", function() {
-            return $(this).attr("src").replace(".svg", ".png");
-        });
+(function() {
+
+    // Method to change states which works in IE7+ / IE8+
+    var btn = document.querySelector('.btn-wrapper');
+
+    // asdf
+    var toggleState = function(elm, att, one, two) {
+        var elm = document.querySelector(elm);
+        elm.setAttribute(att, elm.getAttribute(att) === one ? two : one);
     };
 
-    //E-mail Ajax Send
-    //Documentation & Example: https://github.com/agragregra/uniMail
-    $("form").submit(function() { //Change
-        var th = $(this);
-        $.ajax({
-            type: "POST",
-            url: "mail.php", //Change
-            data: th.serialize()
-        }).done(function() {
-            alert("Thank you!");
-            setTimeout(function() {
-                // Done Functions
-                th.trigger("reset");
-            }, 1000);
-        });
-        return false;
-    });
+    // setup
+    btn.setAttribute('aria-pressed', 'false');
+
+    // turn active state on or off
+    btn.onclick = function(e) {
+        toggleState('.btn-burger', 'data-state', 'off', 'on');
+        toggleState('.btn-wrapper', 'aria-pressed', 'false', 'true');
+        e.preventDefault();
+        $(".menu").slideToggle();
+    }
+
+})();
 
 
-    // no dragstart img
-    $("img, a").on("dragstart", function(event) { event.preventDefault(); });
-
-});
 
 
-//preloader
-$(window).load(function() {
-
-    $(".loader_inner").fadeOut();
-    $(".loader").delay(400).fadeOut("slow");
-
-});
