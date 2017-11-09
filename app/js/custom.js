8899405668dd5d3aba061beefcac5f2c2f0fa945
Alexandr <alexandrfedorenkoooo@gmail.com>
2017-11-09 17:07:08 +0200

    add 60% index page

--- app/js/custom.js
+++ app/js/custom.js
@@ -26,7 +26,40 @@ $(function() {
     }
 
 })();
-
+$('.mainSlider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    arrows:true,  
+    prevArrow: '<button type="button" class="slidArr arrLeft"><img src="img/arr.png" alt="arrow"></button>',
+    nextArrow: '<button type="button" class="slidArr arrRight"><img src="img/arr.png" alt="arrow"></button>',
+    slidesToScroll: 2,
+    responsive: [
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: false,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
 
 
 
