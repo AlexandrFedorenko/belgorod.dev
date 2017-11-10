77c1d547404be103143e079086278aae12e8af9f
Alexandr <alexandrfedorenkoooo@gmail.com>
2017-11-10 17:09:18 +0200

    add page contact 50%

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,31 +1,40 @@
+
+(function() {
+// Method to change states which works in IE7+ / IE8+
+    var btn = document.querySelector('.btn-wrapper');
+
+// asdf
+    var toggleState = function(elm, att, one, two) {
+        var elmm = document.querySelector(elm);
+        elmm.setAttribute(att, elmm.getAttribute(att) === one ? two : one);
+    };
+
+// setup
+    btn.setAttribute('aria-pressed', 'false');
+
+// // turn active state on or off
+//     btn.onclick = function(e) {
+//         e.preventDefault();
+//         toggleState('.btn-burger', 'data-state', 'off', 'on');
+//         toggleState('.btn-wrapper', 'aria-pressed', 'false', 'true');
+//         // $(".menu").slideToggle();
+//     }
+
+$('.btn-wrapper').on('click',function (e) {
+    e.preventDefault();
+   $('.btn-burger').toggleClass('btn-burger__cl');
+
+    $(".menu").slideToggle();
+
+})
+
+})();
 $(function() {
     // no dragstart img
     $("img, a").on("dragstart", function(event) { event.preventDefault(); });   
 });
 
-(function() {
 
-    // Method to change states which works in IE7+ / IE8+
-    var btn = document.querySelector('.btn-wrapper');
-
-    // asdf
-    var toggleState = function(elm, att, one, two) {
-        var elm = document.querySelector(elm);
-        elm.setAttribute(att, elm.getAttribute(att) === one ? two : one);
-    };
-
-    // setup
-    btn.setAttribute('aria-pressed', 'false');
-
-    // turn active state on or off
-    btn.onclick = function(e) {
-        toggleState('.btn-burger', 'data-state', 'off', 'on');
-        toggleState('.btn-wrapper', 'aria-pressed', 'false', 'true');
-        e.preventDefault();
-        $(".menu").slideToggle();
-    }
-
-})();
 $('.mainSlider').slick({
     infinite: true,
     slidesToShow: 4,
@@ -60,6 +69,58 @@ $('.mainSlider').slick({
         }
     ]
 });
+
+
+// Options
+var options = {
+    offset: 110
+};
+
+// Create a new instance of Headhesive.js and pass in some options
+var header = new Headhesive('.header', options);
+
+
+// button to top
+
+
+$(function() {
+
+    $(window).scroll(function() {
+
+        if($(this).scrollTop() != 0) {
+
+            $('#toTop').fadeIn();
+
+        } else {
+
+            $('#toTop').fadeOut();
+
+        }
+
+    });
+
+    $('#toTop').click(function() {
+
+        $('body,html').animate({scrollTop:0},800);
+
+    });
+
+});
+
+//popup
+$('.popup-with-form').magnificPopup({
+    type: 'inline',
+    focus: '#name2'
+});
+
+//mask input
+
+jQuery(function($){
+    $(".phone").mask("+7(999) 999-99-99");    
+});
+
+
+
 
 
 
